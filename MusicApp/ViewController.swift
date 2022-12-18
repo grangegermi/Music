e3ddafae8e7a9a55fcaeb00d9820010f4b30748f
grangegermi <dasha.voloshina@gmail.com>
2022-12-18 16:20:04 +0300

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Даша Волошина on 18.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
